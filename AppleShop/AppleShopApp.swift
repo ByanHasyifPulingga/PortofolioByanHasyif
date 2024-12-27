4a579702f339f1552cd352a45c87897dfbd9c6a2
pajansgaj <byanhasyif@gmail.com>
2024-12-27 22:01:14 +0700

    Initial Commit

---
+++ AppleShop/AppleShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleShopApp.swift
+//  AppleShop
+//
+//  Created by Byan Hasyif on 27/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleShopTests/AppleShopTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppleShopTests.swift
+//  AppleShopTests
+//
+//  Created by Byan Hasyif on 27/12/24.
+//
+
+import Testing
+@testable import AppleShop
+
+struct AppleShopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
